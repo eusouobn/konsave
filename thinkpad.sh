08b718affd05c1745b4662f797e69deca8e40cf6
eusouobn <86791806+eusouobn@users.noreply.github.com>
2022-06-07 15:18:38 -0300

    Update thinkpad.sh

--- thinkpad.sh
+++ thinkpad.sh
@@ -6,6 +6,8 @@ mkdir ~/.config/konsave/profiles
 
 mv ~/konsave/thinkpad.tar.gz ~/.config/konsave/profiles/
 
+sleep 1
+
 tar -xvf ~/.config/konsave/profiles/thinkpad.tar.gz
 
 rm -r ~/.config/konsave/profiles/thinkpad.tar.gz
